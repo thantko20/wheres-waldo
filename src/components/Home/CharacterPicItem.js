4c008389840190dee4cef7dded06b4fb5581b2bf
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-28 22:00:31 +0630

    Separate character avaters from Home.js into components

---
+++ src/components/Home/CharacterPicItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Image, Text } from '@chakra-ui/react';
+
+const CharacterPicItem = ({ imgSrc, label }) => {
+  return (
+    <Box borderRadius="md" overflow="hidden" bg="red.500">
+      <Image boxSize="150px" src={imgSrc} alt={label} fallback />
+      <Text align="center" color="white">
+        label
+      </Text>
+    </Box>
+  );
+};
+
+export default CharacterPicItem;
