9728c2254a0b7a193e45ee263b45393628d77f45
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-29 21:45:20 +0630

    PopUp can now be rendered on the clicked position

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,41 +1,98 @@
-import { Container, Box, Image } from '@chakra-ui/react';
-import React from 'react';
+import {
+  Container,
+  Box,
+  Image,
+  Heading,
+  Flex,
+  VStack,
+  Text,
+  Button,
+} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 import GameImg from '../../assets/game-image.jpg';
 import {
+  calculatePos,
   calculateRelativeCoordinateOnClickEvent,
   isInTheHitBox,
 } from '../../utils';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
+import { getCharacters, getHitBox } from '../../firebase/dbHelper';
+
+const PopUp = ({ names, isActive, position: { top, left } }) => {
+  if (isActive) {
+    return (
+      <Flex
+        direction="column"
+        gap={8}
+        pos="absolute"
+        top={top + 'px'}
+        left={left + 'px'}
+        bg="white"
+        color="gray.900"
+      >
+        <Heading as="h3">Who&apos; this?</Heading>
+        <VStack>
+          {names.map((name, idx) => (
+            <Button key={idx} variant="link" color="inherit">
+              <Text>{name[0].toUpperCase() + name.slice(1)}</Text>
+            </Button>
+          ))}
+        </VStack>
+      </Flex>
+    );
+  }
+};
 
 const Game = () => {
-  const handleOnCharacterChoice = async (characterName, e) => {
-    const coordinate = calculateRelativeCoordinateOnClickEvent(e);
-    const ref = doc(db, 'characters', characterName.toLowerCase());
+  const [characters, setCharacters] = useState([]);
 
-    const characterDoc = await getDoc(ref);
-    const hitBox = {
-      horizontalHitBox: characterDoc.data().horizontalHitBox,
-      verticalHitBox: characterDoc.data().verticalHitBox,
+  const [hasClickedOnImg, setHasClickedOnImg] = useState(false);
+  const [popUpPos, setPopUpPos] = useState({ top: 0, left: 0 });
+  const [isPopupActive, setIsPopUpActive] = useState(false);
+
+  useEffect(() => {
+    const retrieveCharacters = async () => {
+      const temp = await getCharacters();
+
+      setCharacters(temp);
     };
 
-    if (isInTheHitBox(hitBox, coordinate)) {
-      alert(`You found ${characterName}!`);
-    } else {
-      alert(`That wasn't ${characterName}.`);
+    retrieveCharacters();
+  }, []);
+
+  const handleOnClick = (e) => {
+    setIsPopUpActive(true);
+    const pos = calculatePos(e);
+    console.log(pos);
+    setPopUpPos({ top: pos[1], left: pos[0] });
+
+    // await handleOnCharacterChoice('characterName', e);
+  };
+
+  const handleOnCharacterChoice = async (characterName, e) => {
+    const coordinate = calculateRelativeCoordinateOnClickEvent(e);
+    console.log(e);
+    try {
+      const hitBox = await getHitBox(characterName);
+
+      if (isInTheHitBox(hitBox, coordinate)) {
+        alert(`You found ${characterName}!`);
+      } else {
+        alert(`That wasn't ${characterName}.`);
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
   return (
     <Box w="full" mb={10}>
-      <Container maxW="1920px">
-        <Image
-          src={GameImg}
-          w="full"
-          onClick={(e) => {
-            handleOnCharacterChoice('waldo', e);
-          }}
+      <Container maxW="1920px" pos="relative">
+        <PopUp
+          names={characters}
+          position={popUpPos}
+          isActive={isPopupActive}
         />
+        <Image src={GameImg} w="full" onClick={handleOnClick} />
       </Container>
     </Box>
   );
