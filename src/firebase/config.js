88bfb2782bf379555e1d4e6a18331dfda07c7d35
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-29 12:34:32 +0630

    Add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDqjcSqUWFINfXnjDcQf-JRy7V7FYmDwYY',
+  authDomain: 'where-s-waldo-1.firebaseapp.com',
+  projectId: 'where-s-waldo-1',
+  storageBucket: 'where-s-waldo-1.appspot.com',
+  messagingSenderId: '719841084700',
+  appId: '1:719841084700:web:8f931cb343110f7cd4e6d9',
+};
