9ce786e1a118e54c975916ed5e1e668d6a6f95c8
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-29 21:03:49 +0630

    Add helper function to retrieve characters from db

--- src/firebase/dbHelper.js
+++ src/firebase/dbHelper.js
@@ -1,4 +1,4 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, query } from 'firebase/firestore';
 import { db } from '.';
 
 export const getHitBox = async (characterName) => {
@@ -8,4 +8,17 @@ export const getHitBox = async (characterName) => {
   const { horizontalHitBox, verticalHitBox } = characterDoc.data();
 
   return { horizontalHitBox, verticalHitBox };
+};
+
+export const getCharacters = async () => {
+  const colRef = collection(db, 'characters');
+  const q = query(colRef);
+
+  const snapShot = await getDocs(q);
+  const characters = [];
+  snapShot.forEach((doc) => {
+    characters.push(doc.data().name);
+  });
+
+  return characters;
 };
