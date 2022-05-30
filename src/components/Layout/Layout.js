dc7b6c9377b0ffb73c3a8dd016e37699bd6fcddf
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-30 21:05:13 +0630

    Add layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,18 @@
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import { Flex, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from './Nav';
 
 const Layout = () => {
   return (
-    <Box bg={useColorModeValue('gray.100', 'gray.900')} minH="100vh">
+    <Flex
+      bg={useColorModeValue('gray.100', 'gray.900')}
+      minH="100vh"
+      direction="column"
+    >
       <Nav />
       <Outlet />
-    </Box>
+    </Flex>
   );
 };
 
