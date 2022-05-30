e1ff31be520a425ae312e88c3eca5dfcefd138ba
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-30 21:04:58 +0630

    Add function for calculating coordinates

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,36 @@
-// Return the coordinate relative to the event target
-export const useRelativeCoordinateOnClickEvent = (event) => {
+export const calculatePos = (event) => {
   const rect = event.currentTarget.getBoundingClientRect();
 
-  const x = event.pageX - rect.left;
-  const y = event.pageY - window.scrollY - rect.top;
+  const top = event.pageX - rect.left;
+  const left = event.pageY - window.scrollY - rect.top;
+
+  return [top, left];
+};
+
+// Return the coordinate relative to the event target
+export const calculateRelativeCoordinateOnClickEvent = (event) => {
+  const [x, y] = calculatePos(event);
+  const rect = event.currentTarget.getBoundingClientRect();
 
   const offsetX = parseFloat(x / rect.width).toFixed(4);
   const offsetY = parseFloat(y / rect.height).toFixed(4);
 
   return [offsetX, offsetY];
+};
+
+// Calculate if the hit is within the hitbox or not
+export const isInTheHitBox = (hitBox, coordinate) => {
+  const { horizontalHitBox, verticalHitBox } = hitBox;
+  const [x, y] = coordinate;
+
+  if (
+    x >= horizontalHitBox[0] &&
+    x <= horizontalHitBox[1] &&
+    y >= verticalHitBox[0] &&
+    y <= verticalHitBox[1]
+  ) {
+    return true;
+  }
+
+  return false;
 };
