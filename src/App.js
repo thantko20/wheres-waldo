be05bd9d1c233d3441d7921bd7cfbe57c9c05e84
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-30 21:05:23 +0630

    Finish up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import Layout from './Layout';
-import Home from './components/Home';
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+import Game from './components/Game/Game';
+import Leaderboard from './components/Leaderboard';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
           </Route>
         </Routes>
       </HashRouter>
